91a316bd31746e204ba5821e92029bbb317199fb
thilakshids <thilakshids@gmail.com>
2021-04-01 08:37:58 +0530

    test7

--- test.py
+++ test.py
@@ -1,23 +1,12 @@
 
-num= 2
-print (num)
+# num= 2
+# print (num)
  
-  list in python
-num=[12,45,34,33,77]
-print (num)
+#   list in python
+# num=[12,45,34,33,77]
+# print (num)
 
-val =[2, 'thilakshi', 2.5]
-print (val)
-# val.pop()
-# print(val)
+# val =[2, 'thilakshi', 2.5]
+# print (val)
 
-# tup=(21,44,56,732)
-
-
-print(tup[2])
-
-s= {22,65,34,78,22,45}
-# print (s)
-print("hello")
- 
 
