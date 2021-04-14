3b3b76cc6a610f27038798ca6e0e1214f6e9e399
thilakshids <thilakshids@gmail.com>
2021-04-14 10:58:17 +0530

    ex 2

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ num=[12,45,34,33,77]
 print (num)
 
 
-val =[2, 'thilakshi', 2.5]
-print (val)
-print ('thilakshi')
-val=[3,'school',2.3]
+# val =[2, 'thilakshi', 2.5]
+# print (val)
+# print ('thilakshi')
+# val=[3,'school',2.3]
 
