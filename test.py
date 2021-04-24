f4bc01f7527db8f58c0446aacce4ac19f49bf567
thilakshids <thilakshids@gmail.com>
2021-04-24 22:35:37 +0530

    ex1

--- test.py
+++ test.py
@@ -2,19 +2,18 @@
 # num= 2
 # print (num)
  
-# #   list in python
-# num=[12,45,34,33,77]
-# print (num)
+#   list in python
+num=[12,45,34,33,77]
+print (num)
 
 
-# # # val =[2, 'thilakshi', 2.5]
-# # # print (val)
-# # # print ('thilakshi')
-# # # val=[3,'school',2.3]
-# print ('thilakshi')
+# # val =[2, 'thilakshi', 2.5]
+# # print (val)
+# # print ('thilakshi')
+# # val=[3,'school',2.3]
+print ('thilakshi')
 
-# val =[3, 'I can do this' , 3.6]
-# print (val)
+val =[3, 'I can do this' , 3.6]
+print (val)
 #  x = int(1)
- y=int(2.8)
- z=int("3")
+
