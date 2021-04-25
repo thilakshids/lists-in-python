edeed96dd8fbda5fa488bb1521ebdfc67ecabcb1
thilakshids <thilakshids@gmail.com>
2021-04-25 08:31:07 +0530

    sum

--- test.py
+++ test.py
@@ -1,19 +1,3 @@
-
-# num= 2
-# print (num)
- 
-#   list in python
-num=[12,45,34,33,77]
-print (num)
-
-
-# # val =[2, 'thilakshi', 2.5]
-# # print (val)
-# # print ('thilakshi')
-# # val=[3,'school',2.3]
-print ('thilakshi')
-
-val =[3, 'I can do this' , 3.6]
-print (val)
-#  x = int(1)
-
+a = (1,3,5,6,7,3)
+x = sum(a)
+print (x)
