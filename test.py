9de22e67b13cadb51570d4b28faba51982a7f943
thilakshids <thilakshids@gmail.com>
2021-04-25 08:35:18 +0530

    intput

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-a = (1,3,5,6,7,3)
-x = sum(a)
-print (x)
+# a = (1,3,5,6,7,3)
+# x = sum(a)
+# print (x)
+ x = input("Enter a number")
+ mod = num %2
+ if mod >0:
+     print("odd number")
+ else:
+     print("Even number")    
