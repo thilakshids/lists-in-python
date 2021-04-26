f09b6714939cefc675f9e911a40898a56f63ff72
thilakshids <thilakshids@gmail.com>
2021-04-26 09:09:03 +0530

    data types

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
-# a = (1,3,5,6,7,3)
-# x = sum(a)
-# print (x)
- x = input("Enter a number")
- mod = num %2
- if mod >0:
-     print("odd number")
- else:
-     print("Even number")    
+a = 5
+print(a, "is a type", type(a))
